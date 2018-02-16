89b845af84a89bb80a9d7cfe3dc82e34fdf7975e
Eric Denovellis <edeno@bu.edu>
2018-02-16 14:01:19 -0500

    Scale

--- regularized_glm/stats.py
+++ regularized_glm/stats.py
@@ -23,4 +23,4 @@ def get_effective_degrees_of_freedom(U):
 def get_coefficient_covariance(U, singular_values, Vt, scale):
     '''Frequentist Covariance Sandwich Estimator'''
     PKt = Vt @ np.diag(1 / singular_values) @ U.T
-    return PKt @ PKt.T
+    return PKt @ PKt.T * scale
