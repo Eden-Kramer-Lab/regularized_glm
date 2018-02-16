f33df9bd61ee0b2a1dc78b66f0bd0a6d57be9b32
Eric Denovellis <edeno@bu.edu>
2018-02-16 11:27:25 -0500

    Add setup code and explanation for package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # regularized_glm
+
+A simple python package for fitting L2- and smoothing-penalized generalized linear models.
+
+Built primarily because statsmodels GLM `fit_regularized` method is built to do elastic net (combination of L1 and L2 penalities), but if you just want to do an L2 or a smoothing penalty (like in generalized additive models), using a penalized iteratively reweighted least squares (p-IRLS) is much faster.
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy >= 1.11', 'statsmodels', 'scipy']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='regularized_glm',
+    version='0.0.1.dev0',
+    license='MIT',
+    description=('L2-penalized generalized linear models'),
+    author='Eric Denovellis',
+    author_email='edeno@bu.edu',
+    url='https://github.com/Eden-Kramer-Lab/regularized_glm',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
